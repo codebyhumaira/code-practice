617d2955cd9b7663b1d6e5687356bb46372c6b88
codebyhumaira <fhumaira255@gmail.com>
2025-07-17 13:09:57 +0500

    Add files via upload

---
+++ table generator .cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	int n;
+	cout<< "Enter number"<<endl;
+	cin>>n;
+	for(i=1;i<=10;i++)
+	{
+	cout<< n<<"*"<< i <<" = "<<n * i<<endl;
+	
+}
+	return 0;
+}
